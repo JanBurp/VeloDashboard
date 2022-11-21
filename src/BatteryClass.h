d735527a0a0fa80d13525bd45486f55e9689bc3a
Jan den Besten <info@jandenbesten.net>
2022-11-21 08:04:15 +0100

    finetuning battery

--- src/BatteryClass.h
+++ src/BatteryClass.h
@@ -30,7 +30,7 @@ public:
         int value = analogRead(this->pin);
         static int meanValue = value;
         meanValue = (meanValue * (V_SAMPLES - 1) + value) / V_SAMPLES;
-        int battery_mv = 3.875 * (VREF / 1023) * meanValue;
+        int battery_mv = 4.3 * (VREF / 1023) * meanValue;
         int cell_mv = battery_mv / NUM_CELLS;
 
         //
